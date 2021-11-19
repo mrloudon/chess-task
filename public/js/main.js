51ce2099e8a8c36c99eed9d3819acb88822a2a9c
Malcolm Loudon <m.r.loudon@gmail.com>
2021-11-19 15:16:56 +1300

    changed gitignore

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 import * as Utility from "./utility.mjs";
 import { doFrontPage } from "./frontPage.mjs";
 import { doPractice } from "./positionPage.mjs";
-const tasks = [doFrontPage, doEthicsPage, doPractice];
+const tasks = [doFrontPage, doEthicsPage, doPrePracticePage, doPractice];
 
 function doEthicsPage(){
     const page = document.getElementById("ethics-page");
@@ -17,6 +17,19 @@ function doEthicsPage(){
     Utility.fadeIn(page);
 }
 
+function doPrePracticePage(){
+    const page = document.getElementById("pre-practice-page");
+    const nextBtn = page.querySelector("button.next-btn");
+
+    function nextBtnClick(){
+        nextBtn.removeEventListener("click", nextBtnClick);
+        Utility.fadeOut(page)
+            .then(nextTask);
+    }
+
+    nextBtn.addEventListener("click", nextBtnClick);
+    Utility.fadeIn(page);
+}
 
 function nextTask(err, result) {
     console.log(`nextTask(${err}, ${result})`);
